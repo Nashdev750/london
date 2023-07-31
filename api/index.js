4f044240c0ae1595f804e58c7ec644f701e86eef
nashdev750 <nashdev750@gmail.com>
2023-07-31 11:41:51 -0400

    build

--- api/index.js
+++ api/index.js
@@ -12,21 +12,22 @@ app.use(cors( {
 }))
 
 const PORT = 4000
-
+app.use(express.json())
 
 app.get('/api/v1/pages', async (req,res)=>{
-    const resp = await axios.get('https://api.londonstockexchange.com/api/v1/pages?path=home&parameters=')
+    console.log(req.query.path)
+    const resp = await axios.get('https://api.londonstockexchange.com/api/v1/pages?path='+req.query.path+'&parameters=')
    
     res.status(200).send(resp.data);
 })
 app.get('/api/gw/feedhandler/token/saml', async (req,res)=>{
-    const resp = await axios.get('https://api.londonstockexchange.com/api/v1/pages?path=home&parameters=')
+    const resp = await axios.get('https://api.londonstockexchange.com/api/gw/feedhandler/token/saml')
    
     res.status(200).send(resp.data);
 })
-app.get('/api/v1/components/refresh', async (req,res)=>{
-    const resp = await axios.get(' https://api.londonstockexchange.com/api/v1/components/refresh')
-   
+app.post('/api/v1/components/refresh', async (req,res)=>{
+    const resp = await axios.post(' https://api.londonstockexchange.com/api/v1/components/refresh',req.body)
+    
     res.status(200).send(resp.data);
 })
 
